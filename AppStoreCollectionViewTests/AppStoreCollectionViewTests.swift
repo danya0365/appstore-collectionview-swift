7b0f6764695f8b2d20c4fe78d8bbbf6be206b6be
marosdee uma <danya.m@excelbangkok.com>
2018-05-10 09:50:17 +0700

    - initial project

---
+++ AppStoreCollectionViewTests/AppStoreCollectionViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  AppStoreCollectionViewTests.swift
+//  AppStoreCollectionViewTests
+//
+//  Created by marosdee on 5/10/18.
+//  Copyright © 2018 excelbangkok. All rights reserved.
+//
+
+import XCTest
+@testable import AppStoreCollectionView
+
+class AppStoreCollectionViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
